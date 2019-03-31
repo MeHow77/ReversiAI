678b95c0678953a96e09d76149b96a6d1ea7bc10
MeHow77 <michal.hawa@gmail.com>
2019-03-31 18:51:04 +0200

    GUI

---
+++ main.py
@@ -0,0 +1,18 @@
+
+from time import time
+import random
+import pickle
+from Reversi import *
+
+game = Reversi(8) #rozmiar planszy kwadratowej
+clock = pygame.time.Clock()
+done = False
+kolor = 1
+while not done:
+    clock.tick(20)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            if(game.press(pygame.mouse.get_pos(), kolor)):
+                kolor *=-1
